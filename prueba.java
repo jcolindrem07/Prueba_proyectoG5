b099c7e69583432e435d1185dd1be8dea183e6af
jc0lindres <88566936+jc0lindres@users.noreply.github.com>
2021-08-06 20:07:29 -0600

    añadir prueba.java

---
+++ prueba.java
@@ -0,0 +1,12 @@
+public class prueba {
+    public static void main(String[] args) {
+        Cola Cola = new Cola("Cola de prueba");
+        Cola.insertar(01);
+        Cola.insertar(02);
+        Cola.insertar(03);
+        Cola.insertar(04);
+        Cola.imprimir();
+        Cola.eliminar();
+        Cola.imprimir();
+    }
+}
